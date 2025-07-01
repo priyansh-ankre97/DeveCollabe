16c8ea19f26f37e342582e6cbfafbbcf41efbe73
priyansh ankre <priyansh@reachmobile.com>
2025-07-01 14:21:30 +0530

    added get user, get all users,delete user,update use apis

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const PORT_NUMBER = 7777;
 
 app.use(express.json());
 
+//add user
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   try {
@@ -13,6 +14,57 @@ app.post("/signup", async (req, res) => {
     res.send("user added successfully!");
   } catch (err) {
     res.status(400).send("error in adding user : " + err);
+  }
+});
+
+//get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length > 0) {
+      res.send(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong : " + err);
+  }
+});
+
+//get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length > 0) {
+      res.send(users);
+    } else {
+      res.status(404).send("Users not found");
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong : " + err);
+  }
+});
+
+//delete a user from database
+app.delete("/user", async (req, res) => {
+  const userId = req.body?.userId;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong : " + err);
+  }
+});
+
+//update a user in a database
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    await User.findByIdAndUpdate(userId, req.body);
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong : " + err);
   }
 });
 
